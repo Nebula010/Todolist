75419357dd34650b2eaf4d9e27ed20232353001a
Nebula010 <Nebula010@users.noreply.github.com>
2019-03-23 18:15:37 +0800

    上传整个文件夹

---
+++ Todolist/src/main/java/com/nebula/todolist/controller/Hello.java
@@ -0,0 +1,13 @@
+package com.nebula.todolist.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
+}
