d2b2d9c474ece5259273a6350679b1520a24c58b
Xuzheng <linxz@garena.com>
2020-03-21 21:20:57 +0800

    Revert "增加index接口"
    This reverts commit c9fafa7d33c83873f31ba517bd4da66165221aac.

--- Todolist/src/main/java/com/nebula/todolist/controller/HelloRestServer.java
+++ Todolist/src/main/java/com/nebula/todolist/controller/HelloRestServer.java
@@ -1,24 +1,19 @@
 package com.nebula.todolist.controller;
 
 import com.nebula.todolist.entity.User;
-import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/rest")
 public class HelloRestServer {
 
-    @PostMapping(value = "/login")
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     public Boolean login(@RequestBody User user) {
         System.out.println("用户名 " + user.getUserName());
         System.out.println("用户密码 " + user.getPassword());
         return true;
-    }
-
-    @GetMapping(value = "/index")
-    public Integer getIndex(@PathParam("value") Integer value) {
-        System.out.println("value: " + value);
-        return value * 2;
     }
 }
