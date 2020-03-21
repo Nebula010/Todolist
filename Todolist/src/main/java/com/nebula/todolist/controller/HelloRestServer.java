c9fafa7d33c83873f31ba517bd4da66165221aac
Nebula <1661198471@qq.com>
2020-03-21 19:29:57 +0800

    增加index接口

--- Todolist/src/main/java/com/nebula/todolist/controller/HelloRestServer.java
+++ Todolist/src/main/java/com/nebula/todolist/controller/HelloRestServer.java
@@ -1,19 +1,24 @@
 package com.nebula.todolist.controller;
 
 import com.nebula.todolist.entity.User;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/rest")
 public class HelloRestServer {
 
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @PostMapping(value = "/login")
     public Boolean login(@RequestBody User user) {
         System.out.println("用户名 " + user.getUserName());
         System.out.println("用户密码 " + user.getPassword());
         return true;
+    }
+
+    @GetMapping(value = "/index")
+    public Integer getIndex(@PathParam("value") Integer value) {
+        System.out.println("value: " + value);
+        return value * 2;
     }
 }
