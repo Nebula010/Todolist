aa639a15085e1c6922f6623ae76e07087ad7bffc
Nebula <1661198471@qq.com>
2020-03-22 20:14:35 +0800

    增加CorsFilter解决跨域问题

--- Todolist/src/main/java/com/nebula/todolist/TodolistApplication.java
+++ Todolist/src/main/java/com/nebula/todolist/TodolistApplication.java
@@ -4,6 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * @author Nebula
@@ -17,4 +21,14 @@ public class TodolistApplication {
         SpringApplication.run(TodolistApplication.class, args);
     }
 
+    @Bean
+    public CorsFilter corsFilter() { //设置跨域
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
 }
